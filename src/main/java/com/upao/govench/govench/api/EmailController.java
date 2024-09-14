d65953927759564a83be4e748e2ffa1b0946f4f1
jaimePalomino <jpalominoc5@upao.edu.pe>
2024-09-14 00:26:50 -0500

    Implementación de la recuperación de la contraseña mediante correo electrónico.

---
+++ src/main/java/com/upao/govench/govench/api/EmailController.java
@@ -0,0 +1,35 @@
+package com.upao.govench.govench.api;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import com.upao.govench.govench.model.dto.EmailRequestDTO;
+import com.upao.govench.govench.service.PasswordResetService;
+import com.upao.govench.govench.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/email")
+public class EmailController {
+
+    @Autowired
+    private PasswordResetService passwordResetService;
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/forgot-password/{email}")
+    public ResponseEntity<String> forgotPassword(@PathVariable("email") String email) {
+        System.out.println(email);
+        return ResponseEntity.ok(passwordResetService.initiatePasswordReset(email));
+    }
+
+    @PostMapping("/reset-password/{token}/{newPass}")
+    public ResponseEntity<String> resetPassword(@PathVariable("token") String token, @PathVariable("newPass") String newPassword) {
+        passwordResetService.resetPassword(token, newPassword);
+        return ResponseEntity.ok("Password reset successful");
+    }
+}
---
+++ src/main/java/com/upao/govench/govench/service/PasswordResetService.java
@@ -0,0 +1,60 @@
+package com.upao.govench.govench.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.upao.govench.govench.model.entity.PasswordResetToken;
+import com.upao.govench.govench.model.entity.User;
+import com.upao.govench.govench.repository.PasswordResetTokenRepository;
+import com.upao.govench.govench.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+public class PasswordResetService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordResetTokenRepository tokenRepository;
+
+    @Autowired
+    private EmailService emailService;
+
+    public String initiatePasswordReset(String email) {
+        if (userRepository.existsByEmail(email)) {
+            User user = userRepository.findByEmailQuery(email);
+            try {
+                String token = UUID.randomUUID().toString();
+                PasswordResetToken passwordResetToken = new PasswordResetToken();
+                passwordResetToken.setToken(token);
+                passwordResetToken.setUser(user);
+                passwordResetToken.setExpiryDate(LocalDateTime.now().plusHours(1));
+                tokenRepository.save(passwordResetToken);
+
+                String titleEmail = "¡Hola, Somos Govench!";
+                String messageEmail = "\n\nHola, solicitaste un restablecimiento de contraseña para tu cuenta "+ email +", \na continuación copia y pega el siguiente token en el formulario para continuar.";
+                String resetLink = titleEmail + messageEmail +" \n\nTOKEN: " + token;
+                emailService.sendEmail(user.getEmail(), "Password Reset Request \uD83D\uDD12", resetLink);
+                return "Token enviado exitosamente";
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Token no enviado "+e.getMessage();
+            }
+        }
+        return "Token no encontrado";
+    }
+
+    public void resetPassword(String token, String newPassword) {
+        PasswordResetToken passwordResetToken = tokenRepository.findByToken(token);
+        if (passwordResetToken != null && passwordResetToken.getExpiryDate().isAfter(LocalDateTime.now())) {
+            User user = passwordResetToken.getUser();
+            user.setPassword(newPassword); // Make sure to hash the password before saving
+            userRepository.save(user);
+            tokenRepository.delete(passwordResetToken);
+        } else {
+            throw new RuntimeException("Invalid or expired password reset token");
+        }
+    }
+}
