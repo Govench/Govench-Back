d0d6e9091292f0f51cce99ca485621f46b29c82e
Miguel Angel Escobar Gomez <maeg2468@gmail.com>
2024-10-19 02:31:48 -0500

    feat:Correcion en publicar contenido comunidad

--- src/main/java/com/upao/govench/govench/repository/PostRepository.java
+++ src/main/java/com/upao/govench/govench/repository/PostRepository.java
@@ -1,10 +1,15 @@
 package com.upao.govench.govench.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.upao.govench.govench.model.entity.Post;
 
+import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    @Query("SELECT p FROM Post p WHERE p.comunidad.id = :communityId")
+    List<Post> findPostsByCommunityId(@Param("communityId") int communityId);
 }
 
